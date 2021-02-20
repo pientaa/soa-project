02235d01f4d8f32212613dbd250f9f22e804f75a
Łukasz Pięta <pieta.lukasz98@gmail.com>
2021-02-20 16:50:57 +0100

    Gateway init

---
+++ gateway/src/test/kotlin/com/pientaa/gateway/GatewayApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.pientaa.gateway
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
