c2cb4419c40d4d901c5fc685052c5800e0a0ea78
Łukasz Pięta <pieta.lukasz98@gmail.com>
2021-02-21 12:33:11 +0100

    Implement delete user method in gateway

--- gateway/src/main/kotlin/com/pientaa/gateway/UserController.kt
+++ gateway/src/main/kotlin/com/pientaa/gateway/UserController.kt
@@ -2,9 +2,12 @@ package com.pientaa.gateway
 
 import com.pientaa.gateway.dto.UserDTO
 import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
@@ -13,6 +16,10 @@ import org.springframework.web.bind.annotation.RestController
 class UserController(
     private val userServiceClient: UserServiceClient
 ) {
+
+    @DeleteMapping
+    fun deleteUser(@RequestParam userId: String) = userServiceClient.deleteUser(userId)
+
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     fun createUser(@RequestBody createUserDTO: UserDTO) = userServiceClient.createUser(createUserDTO)
--- gateway/src/main/kotlin/com/pientaa/gateway/UserServiceClient.kt
+++ gateway/src/main/kotlin/com/pientaa/gateway/UserServiceClient.kt
@@ -3,6 +3,8 @@ package com.pientaa.gateway
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import com.pientaa.gateway.dto.UserDTO
+import okhttp3.HttpUrl
+import okhttp3.HttpUrl.Companion.toHttpUrlOrNull
 import okhttp3.MediaType
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
@@ -22,13 +24,13 @@ class UserServiceClient {
         val mapper = jacksonObjectMapper().apply {
             registerKotlinModule()
         }
+        val client = OkHttpClient()
     }
 
     @Value("\${services.user.url}")
     lateinit var userServiceUrl: String
 
     fun createUser(createUserDTO: UserDTO) {
-        val client = OkHttpClient()
 
         val json = mapper.writeValueAsString(createUserDTO)
 
@@ -40,6 +42,24 @@ class UserServiceClient {
 
         client.newCall(request).execute().use { response ->
             log.info("Create user { ${createUserDTO.firstName}, ${createUserDTO.lastName} } request")
+            log.info("Client response: ${response.code}, ${response.message}")
+        }
+    }
+
+    fun deleteUser(userId: String) {
+
+        val urlBuilder: HttpUrl.Builder = userServiceUrl.toHttpUrlOrNull()!!.newBuilder()
+        urlBuilder.addQueryParameter("userId", userId)
+
+        val url: String = urlBuilder.build().toString()
+
+        val request: Request = Request.Builder()
+            .url(url)
+            .delete()
+            .build()
+
+        client.newCall(request).execute().use { response ->
+            log.info("Delete user { $userId } request")
             log.info("Client response: ${response.code}, ${response.message}")
         }
     }
